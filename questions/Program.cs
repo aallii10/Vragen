69335dccbfa0b1bdc9911c4d90e6ee53e9f7014e
aallii10 <17014859@student.hhs.nl>
2018-11-18 17:40:26 +0100

    1.0

--- questions/Program.cs
+++ questions/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace questions
 {
@@ -6,7 +8,56 @@ namespace questions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IList<Vragen> vraagArray = new List<Vragen>(){
+
+                new Vragen() {Vraag = "sadsasa", Antwoord = "", MoeilijkheidsGraad = "1", Categorie = ""},
+                new Vragen() {Vraag = "sadfdvcx", Antwoord = "", MoeilijkheidsGraad = "2", Categorie = ""},
+                new Vragen() {Vraag = "3243", Antwoord = "", MoeilijkheidsGraad = "3", Categorie = ""},
+                new Vragen() {Vraag = "yhytjthg", Antwoord = "", MoeilijkheidsGraad = "1", Categorie = ""},
+                new Vragen() {Vraag = "adqwsad", Antwoord = "", MoeilijkheidsGraad = "2", Categorie = ""}
+            };
+
+
+            //Vragen vragens = vraagArray.Where(v => v.MoeilijkheidsGraad == "2").FirstOrDefault();
+
+         
+
+           /* foreach(var v in result)
+            {
+                Console.WriteLine(v.Vraag);
+            }*/
+
+
+            
+
+            //Console.WriteLine("Kies een moeilijkheidsgraad: 1, 2, 3");
+
+            string read = Console.ReadLine();
+
+            if(read.Equals("1"))
+            {
+                var result = from v in vraagArray
+                             where v.MoeilijkheidsGraad.Equals("1")
+                             select v;
+
+                Console.WriteLine(result.v.Vraag);
+            }
+
+            else if (read.Equals("2"))
+            {
+                Console.WriteLine("");
+            }
+
+            else if (read.Equals("3"))
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("deze moeilijkheidsgraad bestaat niet.");
+            }
+
+
         }
     }
 }
---
+++ questions/Vragen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace questions
+{
+    class Vragen
+    {
+        public string Vraag { get; set; }
+        public string Antwoord { get; set; }
+        public string MoeilijkheidsGraad { get; set; }
+        public string Categorie { get; set; }
+    }
+}
